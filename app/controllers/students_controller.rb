2adea4bc3a396019b26970534fd60db3cea4dde9
nptravis <nptravis@yahoo.com>
2018-04-04 14:26:45 +0700

    got some graphs working?

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,9 +6,25 @@ class StudentsController < ApplicationController
 
 	def show
 		@student = Student.find(params[:id])
-		@data_array = []
+		@data_hash = {}
+		@hom_array = [0, 0, 0, 0]
 		@student.grades.each do |grade|
-			@data_array << {name: grade.standard.standardname, data: grade.percent.to_s}
+			if grade.percent
+				case grade.standard.standardname
+				when "Responsibility"
+					@hom_array[0] = grade.percent.round
+				when "Engagement"
+					@hom_array[1] = grade.percent.round
+				when "Reflection"
+					@hom_array[2] = grade.percent.round
+				when "Respect"
+					@hom_array[3] = grade.percent.round
+				else
+					@data_hash.merge!(grade.standard.standardname => grade.percent.to_s)
+				end
+			end
 		end
 	end
+
+
 end
