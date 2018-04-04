0c827d329727c79e18c199a3310a21287503c97f
nptravis <nptravis@yahoo.com>
2018-04-04 12:00:31 +0700

    adding some chart functionality to show page for students)

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,9 +1,14 @@
 class StudentsController < ApplicationController
 	def index
+		@students1 = Student.all
 		@students = Student.all.sort_by {|student| student.lastfirst}
 	end
 
 	def show
 		@student = Student.find(params[:id])
+		@data_array = []
+		@student.grades.each do |grade|
+			@data_array << {name: grade.standard.standardname, data: grade.percent.to_s}
+		end
 	end
 end
