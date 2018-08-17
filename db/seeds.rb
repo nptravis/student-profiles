d35a0e2d026ce53d9171c11e00687f2ded47628c
Nicolas Travis <home@Nicolass-MacBook-Pro.local>
2018-08-17 10:19:35 +0700

    create new migration files and exported new data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,39 +1,46 @@
 
 # ///////// Simple data
-file = File.read('data/all-standards.json')
+file = File.read('data/all-final-standards.json')
 # file = File.read('data/data.json')
 data_hash = JSON.parse(file)
 data_hash["items"].each do |set|
 	
-	student = Student.find_or_create_by(lastfirst: set["lastfirst"], dcid: set["studentsdcid"], student_number: set["student_number"], gradelevel: set["gradelevel"])
-
-	standard = Standard.find_or_create_by(dcid: set["standardid"], standardname: set["name"], identifier: set["identifier"])
-
-	course = Course.find_or_create_by(course_name: set["course_name"], course_number: set["course_number"])
+	student = Student.find_or_create_by(
+		lastfirst: set["lastfirst"], 
+		student_number: set["student_number"], 
+		gradelevel: set["gradelevel"])
 	
-	if standard.standardname != "Semester Comment"
-		grade = Grade.new(course_id: course.id, standard_id: standard.id, student_id: student.id, grade: set["calculatedmostrecentscores"], modified: set["whenmodified"], semester: set["storecode"])
+	
+	standard = Standard.find_or_create_by(
+		standardname: set["name"], 
+		identifier: set["identifier"])
 
-		if !grade.course.instructor
-			grade.course.instructor = set["whocreated"]
-		end
+	course = Course.find_or_create_by(
+		course_name: set["course_name"], 
+		course_number: set["course_number"],
+		section: set["section_number"],
+		teacher: set["lastfirst_1"])
+	
+	grade = Grade.new(
+		course_id: course.id, 
+		standard_id: standard.id, 
+		student_id: student.id, 
+		grade: set["standardgrade"],  
+		semester: set["storecode"])
 
-		case grade.grade
-			when "E"
-				grade.grade = 4
-			when "M"
-				grade.grade = 3
-			when "P"
-				grade.grade = 2
-			when "B"
-				grade.grade = 1
-			when nil 
-				grade.grade = 0
-			else
-				grade.grade = 0
-		end
-
-		grade.save
+	case grade.grade
+		when "E"
+			grade.grade = 4
+		when "M"
+			grade.grade = 3
+		when "P"
+			grade.grade = 2
+		when "B"
+			grade.grade = 1
+		else
+			grade.grade = 0
 	end
-	
+
+	grade.save
+
 end
