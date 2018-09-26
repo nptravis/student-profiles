0a4aa4ca02edb006488de30c8f0e3d33d52662ab
nptravis <nptravis@yahoo.com>
2018-09-26 11:30:52 +0700

    getting ready to seed heroku db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-# Dir[File.join(Rails.root, 'db', 'seeds', 'semester_comment_seeds.rb')].sort.each do |seed|
-#   load seed
-# end
+Dir[File.join(Rails.root, 'db', 'seeds', 'semester_comment_seeds.rb')].sort.each do |seed|
+  load seed
+end
 
